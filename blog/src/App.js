081d7edaa4adc8f85b874fa9b2b55396268cf1a0
하진 <mz-hazzing@lfpartners.co.kr>
2023-05-04 16:50:06 +0900

    0504 Component: 많은 div들을 한 단어로 줄이고 싶으면

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,9 +1,9 @@
-/*eslint-disable*/
-import logo from './logo.svg';
-import './App.css';
-import {useState} from "react";
+    /*eslint-disable*/
+    import logo from './logo.svg';
+    import './App.css';
+    import {useState} from "react";
 
-function App() {
+    function App() {
 
     //변수와 state의 차이는? : state는 값이 변경되었을때 html이 자동 재랜더링됨, 자주 변경될 값들은 state로 만드는게 좋음
 
@@ -27,38 +27,64 @@ function App() {
 * 기존state == 신규state : 변경해주지 않음
 * state가 array/object면 독립적 카피본을 만들어서 수정해야함
 * */
-  return (
-      //JSX 문법
-    <div className="App">
-        <div className="black-nav">
-            <h4 style={{color:'red', fontSize:'16px'}}>{ post }</h4>
+    return (
+        //JSX 문법
+        <div className="App">
+            <div className="black-nav">
+                <h4 style={{color: 'red', fontSize: '16px'}}>{post}</h4>
+            </div>
+            <button onClick={() => {
+                let titles = [...글제목];
+                titles.sort();
+                changeTitle(titles);
+            }}>정렬
+            </button>
+            <button onClick={() => {
+                // 글제목[0] = '여자 코트 추천';   //원본 데이터는 보존하는게 좋음 array를 수정했지 변수에 있던 화살표는 수정되지 않음
+                // let copy = 글제목; // 글제목 이라는 변수에 저장된건 ram에서 위치를 나타내는 화살표임  화살표가 똑같으니까 기존과 신규가 같아서 state 변경이 안됨
+                let copy = [...글제목];  // 데이터를 복제해서 사용
+                copy[0] = '여자 코트 추천';
+                // changeTitle(['여자 코트 추천','강남 우동맛집','파이썬독학','여자 코트 추천']) // 이런 방식은 효율성이 떨어짐
+                changeTitle(copy);
+            }}>글수정
+            </button>
+            <div className="list">
+                <h4>{글제목[0]} <span onClick={() => {
+                    changeLike(like + 1)
+                }}>👍</span> {like} </h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{글제목[1]}</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <div className="list">
+                <h4>{글제목[2]}</h4>
+                <p>2월 17일 발행</p>
+            </div>
+            <Modal></Modal>
+            <Modal/>
         </div>
-        <button onClick={()=>{
-            let titles = [...글제목];
-            titles.sort();
-            changeTitle(titles);}}>정렬</button>
-        <button onClick={()=>{
-            // 글제목[0] = '여자 코트 추천';   //원본 데이터는 보존하는게 좋음 array를 수정했지 변수에 있던 화살표는 수정되지 않음
-            // let copy = 글제목; // 글제목 이라는 변수에 저장된건 ram에서 위치를 나타내는 화살표임  화살표가 똑같으니까 기존과 신규가 같아서 state 변경이 안됨
-            let copy = [...글제목];  // 데이터를 복제해서 사용
-            copy[0] = '여자 코트 추천';
-            // changeTitle(['여자 코트 추천','강남 우동맛집','파이썬독학','여자 코트 추천']) // 이런 방식은 효율성이 떨어짐
-            changeTitle(copy);
-        }}>글수정</button>
-        <div className="list">
-            <h4>{ 글제목[0] } <span onClick={()=>{changeLike(like+1)}}>👍</span> {like} </h4>
-            <p>2월 17일 발행</p>
-        </div>
-        <div className="list">
-            <h4>{ 글제목[1] }</h4>
-            <p>2월 17일 발행</p>
-        </div>
-        <div className="list">
-            <h4>{ 글제목[2] }</h4>
-            <p>2월 17일 발행</p>
-        </div>
-    </div>
-  );
-}
+        );
+    }
 
-export default App;
+    /*
+    * 컴포넌를 쓰면 좋은 상황들!
+    * 반복적인 html축약할때
+    * 큰 페이지들
+    * 자주 변경되는 것들
+    * */
+
+    function Modal() { // 컴포넌트 생성
+        return (
+            <div className="modal">
+                <h4>제목</h4>
+                <p>날짜</p>
+                <p>상세내용</p>
+            </div>
+        )
+    }
+
+
+
+    export default App;
