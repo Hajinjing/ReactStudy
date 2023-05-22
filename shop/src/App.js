a1aa197d4487deecdd9d17dcd8ded48bcf645111
하진 <mz-hazzing@lfpartners.co.kr>
2023-05-22 19:41:23 +0900

    0522 리액트 라우터 1 : 셋팅이랑 기본 라우팅

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,12 +5,11 @@ import bg from './img/bg.png'
 // import ddd from './data.js';
 // import {a, b} from './data.js'; // 중괄호로 가져올때는 export의 변수명을 그대로 가져와서 써야조
 import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
 
     let [shoes] = useState(data)
-    let [ttttt] = useState("https://codingapple1.github.io/shop/shoes1.jpg")
-
 
     return (
         <div className="App">
@@ -18,37 +17,29 @@ function App() {
                 <Container>
                     <Navbar.Brand href="#home">HAZZING WORLD</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="#features">Cart</Nav.Link>
-                        <Nav.Link href="#pricing">About</Nav.Link>
+                        <Nav.Link href="/detail">About</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
+            <Routes>
+                <Route path={"/"} element={
+                    <>
+                    <div className="main-bg" style={{backgroundImage: 'url(' + bg + ')'}}></div>
+                    <div className="container">
+                        <div className="row">
+                            {
+                                <ItemInfo shoes={shoes}/>
+                            }
 
-            <div className="main-bg" style={{backgroundImage: 'url(' + bg + ')'}}></div>
+                        </div>
+                    </div>
+                    </>
+                }/>
+                <Route path={"/detail"} element={<ItemDetail/>} />
+            </Routes>
 
-            <div className="container">
-                <div className="row">
-                    {
-                        <ItemInfo shoes={shoes}/>
-                    }
-                    {/*<div className="col-md-4">*/}
-                    {/*    /!*<img src={process.env.PUBLIC_URL + './logo192.png'} width="80%"/> /!* public폴더 사용해서 이미지 넣는법*!/*!/*/}
-                    {/*    <img src={shoes[0].imageUrl} width="80%"/>*/}
-                    {/*    <h4>{shoes[0].title}</h4>*/}
-                    {/*    <p>{shoes[0].price}</p>*/}
-                    {/*</div>*/}
-                    {/*<div className="col-md-4">*!/*/}
-                    {/*    <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>*/}
-                    {/*    <h4>상품명</h4>*/}
-                    {/*    <p>상품정보</p>*/}
-                    {/*</div><div className="col-md-4">*/}
-                    {/*    <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>*/}
-                    {/*    <h4>상품명</h4>*/}
-                    {/*    <p>상품정보</p>*/}
-                    {/*</div>*/}
-                </div>
-            </div>
 
         </div>
     );
@@ -76,6 +67,24 @@ function ItemInfo(props) {
 
         )
 
+}
+
+function ItemDetail() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                </div>
+                <div className="col-md-6">
+                    <h4 className="pt-5">상품명</h4>
+                    <p>상품설명</p>
+                    <p>120000원</p>
+                    <button className="btn btn-danger">주문하기</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
