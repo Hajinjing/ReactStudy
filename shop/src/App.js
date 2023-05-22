f3f2a7be8812ef08cf201a396ebe9e6a7a3cf8ef
하진 <mz-hazzing@lfpartners.co.kr>
2023-05-22 20:07:30 +0900

    0522 리액트 라우터 2 : navigate, nested routes, oulet

--- shop/src/App.js
+++ shop/src/App.js
@@ -5,11 +5,13 @@ import bg from './img/bg.png'
 // import ddd from './data.js';
 // import {a, b} from './data.js'; // 중괄호로 가져올때는 export의 변수명을 그대로 가져와서 써야조
 import data from './data.js';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import ItemDetail from "./routes/Detail";
 
 function App() {
 
     let [shoes] = useState(data)
+    let navigate = useNavigate();
 
     return (
         <div className="App">
@@ -17,9 +19,11 @@ function App() {
                 <Container>
                     <Navbar.Brand href="#home">HAZZING WORLD</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link onClick={()=>{ navigate("/")}}>Home</Nav.Link>
+                        {/*<Nav.Link onClick={()=>{ navigate(-1)}}>뒤로갸기</Nav.Link>*/}
+                        {/*<Nav.Link href="/">Home</Nav.Link>*/}
                         <Nav.Link href="#features">Cart</Nav.Link>
-                        <Nav.Link href="/detail">About</Nav.Link>
+                        <Nav.Link onClick={()=>{ navigate("/detail")}}>About</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
@@ -38,11 +42,32 @@ function App() {
                     </>
                 }/>
                 <Route path={"/detail"} element={<ItemDetail/>} />
+
+                {/*nested routes /about/member 로 접근했을때, element가 2개 보임*/}
+                <Route path={"/about"} element={<About/>}>
+                    <Route path={"member"} element={<div>멤버임</div>} /> {/* 어디에 보여줄지 정하려면 <Outlet> */}
+                    <Route path={"location"} element={<div>회사들 위치</div>} />
+                </Route>
+
+                {/*<Route path={"/about/member"} element={<About/>}/>*/}
+                {/*<Route path={"/about/location"} element={<About/>}/>*/}
+
+                <Route path={"/about"} element={<About/>} />
+                <Route path={"*"} element={<div>존재하지 않는 페이지입니다.</div>} />
             </Routes>
 
 
         </div>
     );
+}
+
+function About() {
+    return (
+        <div>
+            <h4>회사정보임</h4>
+            <Outlet></Outlet> {/* nested routes의 element 보여주는 곳 */}
+        </div>
+    )
 }
 
 function ItemInfo(props) {
@@ -69,22 +94,6 @@ function ItemInfo(props) {
 
 }
 
-function ItemDetail() {
-    return (
-        <div className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
-                </div>
-                <div className="col-md-6">
-                    <h4 className="pt-5">상품명</h4>
-                    <p>상품설명</p>
-                    <p>120000원</p>
-                    <button className="btn btn-danger">주문하기</button>
-                </div>
-            </div>
-        </div>
-    )
-}
+
 
 export default App;
